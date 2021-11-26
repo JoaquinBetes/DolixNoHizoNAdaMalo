3e6133aa219d08320e5b12054bb7cb61cc7169ed
Joaquin Betes <84933169+JoaquinBetes@users.noreply.github.com>
2021-11-25 21:46:23 -0300

    Update tattoosBoxes.js

--- tattoosBoxes.js
+++ tattoosBoxes.js
@@ -6,7 +6,7 @@ let documentFragment = document.createDocumentFragment();
 
 for(let i = 0; i < 12; i++){
 	let listaTatoos=["mob100.jpg","skull.jpg","legoshi.jpg","uwu.jpg","sesshomaru.jpg","makima.jpg","bs.jpg","mazinger.jpg","rei.jpg","HxH.jpg","rocklee.jpg","skull.jpg"];
-	let listaImgTatoos=["mob100.jpg","calavera.jpg","legoshi.jpg","uwu.jpg","sesshomaru.jpg","makima.jpg","bs.jpg","mazinger.jpg","rei.jpg","HxH.jpg","rocklee.jpg","skull.jpg"];
+	let listaImgTatoos=["mob100.jpg","calavera.jpg","legoshi.jpg","uwu.jpg","sesshomaru.jpg","makima.jpg","bs.jpg","mazinger.jpg","rei.jpg","hxh.jpg","rocklee.jpg","skull.jpg"];
 	let tattoosBox = document.createElement("DIV");
 	tattoosBox.classList.add("tattoos-box");
 	tattoosBox.innerHTML = `<img src="imagenes\\tatoos\\${listaImgTatoos[i]}" class = "tattoos-box__img" loading="lazy">`;
